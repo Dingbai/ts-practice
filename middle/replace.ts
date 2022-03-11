796b8f78f9fdc421e4607259c7cf3f4fd9bceff6
dy <you@example.com>
2022-03-11 13:36:35 +0800

    upd: replaceAll

--- middle/replace.ts
+++ middle/replace.ts
@@ -1,4 +1,4 @@
 namespace Replace {
     type a = Replace<'o', 'b', 'hello'>
-    type Replace<S extends string, R extends string, T extends string> = T extends `${infer F}${S}${infer U}` ? `${F}${R}${U}` : never
+    type Replace<S extends string, R extends string, T extends string> = T extends `${infer F}${S}${infer U}` ? `${F}${R}${U}` : T
 }
