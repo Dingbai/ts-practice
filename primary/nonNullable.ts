1e7f1fd6faa487cb6906e2afc2dcf31ef85284c0
dingye <dingye@dingtalk.com>
2022-04-01 22:05:55 +0800

    feat: 新增nonnullable

---
+++ primary/nonNullable.ts
@@ -0,0 +1,11 @@
+// 去除联合类型 中的 null 和 undefined
+namespace NonNullable {
+    type a = number | null;
+    type b = number | null | undefined;
+    type c = string | undefined;
+    type NonNullable<T> = T extends null | undefined ? never : T;
+
+    type a1 = NonNullable<a>;
+    type b1 = NonNullable<b>;
+    type c1 = NonNullable<c>;
+}
